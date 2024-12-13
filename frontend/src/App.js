a23132ae569ef35c687d490aedbcfd7c7f00e705
IchgehoernurmirZ <Zoey42zhang@gmail.com>
2024-12-13 15:44:10 -0500

    Create a simple Header to start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from '../src/components/Header'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to ClimbLikeANoob</h1>
+        </Container>
+      </main>
+    </>
   )
 }
 
